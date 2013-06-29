de11c192785c6233067566fc05d29afab5300505
Saul St. John <sstjohn@cs.wisc.edu>
2013-06-29 14:14:59 -0500

    add eliminator

---
+++ eliminate.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+
+import bisect
+import os
+import random
+import sys
+import Corewar, Corewar.Benchmarking
+
+COMPETITORS_PER_TOURN = 20
+GAMES_PER_PAIR = 4
+ROUNDS_PER_GAME = 10
+COMPETITORS_DIR = "winners"
+ELIMINATION_SOFT_CUTOFF = 1.5
+ELIMINATION_HARD_CUTOFF = 3
+COMPETITORS_TO_ELIMINATE = 10
+MIN_USEFUL_STD_DEV = 100
+
+def destroy_competitor(warrior):
+	print "destroying %s (with score %d)" % (warrior[0], warrior[1])
+	os.remove(warrior[0])
+
+def eliminate_failures(results):
+	avg = float(reduce(lambda x, y: x + y, map(lambda x: x[1], results))) / float(len(results))
+	std_dev = (float(reduce(lambda x, y: x + y, map(lambda x: (float(x[1]) - avg) ** 2, results))) / float(len(results) - 1)) ** 0.5
+	print "average score is %4.03d" % avg
+	print "standard deviation is %4.03d" % std_dev 
+	if std_dev < MIN_USEFUL_STD_DEV:
+		print "insufficient deviation in results. results voided."
+		return 0
+
+	competitors_destroyed = 0
+	while(results[-1][1] < (avg - (float(ELIMINATION_SOFT_CUTOFF) * std_dev))):
+		if False and results[-1][1] < (avg - (float(ELIMINATION_HARD_CUTOFF) * std_dev)):
+			destroy_competitor(results.pop())
+			competitors_destroyed += 1
+		else:
+			destruction_num = (((avg - float(results[-1][1])) / std_dev) - float(ELIMINATION_SOFT_CUTOFF))
+			destruction_denom = float(ELIMINATION_HARD_CUTOFF) - float(ELIMINATION_SOFT_CUTOFF)
+			print "destruction probibility for %s is %4.02f / %04.02f" % (results[-1][0], destruction_num, destruction_denom)
+			if random.random() < (destruction_num / destruction_denom):
+				destroy_competitor(results.pop())
+				competitors_destroyed += 1
+			else:
+				break
+	return competitors_destroyed
+
+def run_comp():
+	parser = Corewar.Parser(coresize=8000,
+								maxprocesses=8000,
+								maxcycles=80000,
+								maxlength=100,
+								mindistance=100,
+								standard=Corewar.STANDARD_88)
+	try:
+		warriors = [[COMPETITORS_DIR + "/" + x, parser.parse_file(COMPETITORS_DIR + "/" + x), 0] 
+				for x in random.sample(os.listdir(COMPETITORS_DIR), COMPETITORS_PER_TOURN)]
+	except Corewar.WarriorParseError, e:
+		print e
+		sys.exit(1)
+
+	pairings = range(1, COMPETITORS_PER_TOURN)
+	for i in range((COMPETITORS_PER_TOURN - 1)):
+		top = [0] + pairings[:(COMPETITORS_PER_TOURN / 2) - 1]
+		bottom = pairings[len(top) - 1:][::-1]
+		for j in range(COMPETITORS_PER_TOURN / 2):
+			top_score_delta, bottom_score_delta = run_games(warriors[top[j]][1], warriors[bottom[j]][1])
+			warriors[top[j]][2] += top_score_delta
+			warriors[bottom[j]][2] += bottom_score_delta
+		pairings.append(pairings.pop(0))
+
+	print
+	print "elimination results!"
+	print "--------------------"
+	print
+	
+	warriors.sort(key=lambda x: x[2], reverse=True)
+	for w in warriors:
+		print "%s: %4d" % (w[0], w[2])
+
+	print
+
+	return eliminate_failures([[x[0], x[2]] for x in warriors])
+
+def run_games(left, right):
+	mars = Corewar.Benchmarking.MARS_88(coresize=8000,
+						maxprocesses=8000,
+						maxcycles=80000,
+						mindistance=100,
+						maxlength=100)
+	left_score = 0
+	right_score = 0
+	for i in range(GAMES_PER_PAIR):
+		if i % 2 == 0:
+			warriors = (left, right)
+		else:
+			warriors = (right, left)
+		results = mars.run(warriors, rounds = ROUNDS_PER_GAME)
+		if i % 2 == 0:
+			left_score += (5 * results[0][0] + results[0][1])
+			right_score += (5 * results[1][0] + results[1][1])
+		else:
+			left_score += (5 * results[1][0] + results[1][1])
+			right_score += (5 * results[0][0] + results[0][1])
+	
+	return left_score, right_score
+
+if __name__ == "__main__":
+	random.seed()
+	destruction_count = 0
+	while destruction_count < COMPETITORS_TO_ELIMINATE:
+		destruction_count += run_comp()
