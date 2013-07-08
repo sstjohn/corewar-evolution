f8de364fec0006afe5d41e925f9e40df28c2afa1
Saul St. John <sstjohn@cs.wisc.edu>
2013-07-08 16:54:30 -0500

    prevent infinite commit loop, redirect some error messages to stdout

--- repeater.sh
+++ repeater.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 export GIT_MERGE_AUTOEDIT=no
 export PYTHONPATH=`pwd`/pycorewar/lib64/python2.6/site-packages/
@@ -13,11 +13,11 @@ if [ 0 -eq $? ]; then
 	success=1
 	git add winners
 	git commit -m "adding winners"
-	git pull -X ours
-	git push
+	git pull -X ours 2>&1
+	git push 2>&1
 	if [ 0 -ne $? ]; then
-		git pull -X ours
-		git push
+		git pull -X ours 2>&1
+		git push 2>&1
 		if [ 0 -eq $? ]; then
 			pushed=1
 		fi
@@ -25,7 +25,8 @@ if [ 0 -eq $? ]; then
 		pushed=1
 	fi
 else
-	git pull -X ours
+	git clean -f winners 2>&1
+	git pull -X theirs 2>&1
 fi
 rm -rf `seq 0 10000`
 if [ $success -eq 1 ]; then
@@ -39,11 +40,11 @@ if [ $success -eq 1 ]; then
 		fi
 		git add -u winners
 		git commit -m "removing losers"
-		git pull -X theirs
-		git push
+		git pull -X theirs 2>&1
+		git push 2>&1
 		if [ 0 -ne $? ]; then
-			git pull -X theirs
-			git push
+			git pull -X theirs 2>&1
+			git push 2>&1
 			if [ 0 -eq $? ]; then
 				pushed=1
 			fi
@@ -51,7 +52,9 @@ if [ $success -eq 1 ]; then
 			pushed=1
 		fi
 	else
-		git pull -X theirs
+		git clean -f winners 2>&1
+		git checkout winners 2>&1
+		git pull -X theirs 2>&1
 	fi
 fi
 if [ ! -f .stopnow ] && [ "$0" == "./repeater.sh" ]; then
@@ -59,9 +62,18 @@ if [ ! -f .stopnow ] && [ "$0" == "./repeater.sh" ]; then
 fi
 if [ $pushed -eq 0 ]; then
 	result=1
-	while [ $result -ne 0 ]; do
-		git pull -X theirs
-		git push
+	tries=0
+	while [ $result -ne 0 ] && [ $tries -ne 10 ]; do
+		git pull -X theirs 2>&1
+		git push 2>&1
 		result=$?
+		tries=$(($tries + 1))
 	done
+	if [ $result -ne 0 ]; then
+		echo "failed to commit results! giving it one last go!" >&2
+		git pull -X theirs >&2
+		git push >&2
+		result=$?
+	fi
+	exit $result
 fi
